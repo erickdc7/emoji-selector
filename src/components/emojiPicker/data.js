19dd0279a8e5e02ede595dbb9f9836d2ba6f1876
ErickDC <erickdiazc717@gmail.com>
2023-02-06 13:24:22 -0500

    data.js component updated

---
+++ src/components/emojiPicker/data.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        symbol: "😀",
+        name: "cara alegre",
+        keyword: "smiling face happy",
+    },
+    {
+        symbol: "💖",
+        name: "corazon",
+        keyword: "heart love like",
+    },
+    {
+        symbol: "🔥",
+        name: "fuego",
+        keyword: "fire hot burn",
+    },
+    {
+        symbol: "💎",
+        name: "diamante",
+        keyword: "gem stone diamond",
+    },
+    {
+        symbol: "🤯",
+        name: "cabeza explotada",
+        keyword: "head exploding mind blowing",
+    }
+];
