73f8da4af41df4975377c86c55d83900e8500ccb
ErickDC <erickdiazc717@gmail.com>
2023-02-06 13:27:40 -0500

    emojiPickerInput.js component created

---
+++ src/components/emojiPicker/emojiPickerInput.js
@@ -0,0 +1,10 @@
+import EmojiPicker from "./emojiPicker";
+
+export default function EmojiPickerInput() {
+    return (
+        <div>
+            <input type="text" />
+            <EmojiPicker />
+        </div>
+    )
+}
